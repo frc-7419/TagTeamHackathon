ecb427537d3a2c6053dcb8d6160a8868c66f6df7
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-12-23 14:38:49 -0800

    random bugs

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,10 +1,11 @@
 public class StepTracker {
-    public static void main(String[] args) {
-        private int minSteps;
-        private int totalSteps;
-        private int numDays;
-        private int numActiveDays;
 
+    private int minSteps;
+    private int totalSteps;
+    private int numDays;
+    private int numActiveDays;
+public static void main(String[] args) {
+        
 
     }
 }
