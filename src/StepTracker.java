1e57cf7bfb246cd54b607a6770ac5504759228eb
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-12-23 14:37:23 -0800

    Fix file bugs

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,22 +1,9 @@
 public class StepTracker {
     public static void main(String[] args) {
-        int minSteps;
-        int totalSteps;
-        int numDays;
-        int numActiveDays;
-        // uncomment the following lines of code to test if you have the correct output
-
-        /*
-        StepTracker tr = new StepTracker(10000);
-        System.out.println("number of active days: " + tr.activeDays());
-        System.out.println("average number of steps: " + tr.averageSteps());
-        tr.addDailySteps(5000);
-        tr.addDailySteps(9000);
-        System.out.println("number of active days: " + tr.activeDays());
-        System.out.println("average number of steps: " +tr.averageSteps());
-        tr.addDailySteps(13000);
-        System.out.println("number of active days: " + tr.activeDays());
-        */
+        private int minSteps;
+        private int totalSteps;
+        private int numDays;
+        private int numActiveDays;
 
 
     }
