436ad48c85dff9bd4e044385d0e24af8df0e1705
Krishiv Manyam <krishivmanyam@gmail.com>
2023-09-14 16:34:08 -0700

    Built Constructor

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -4,4 +4,11 @@ public class StepTracker {
     private int numDays;
     private int numActiveDays;
 
+    public StepTracker(int minSteps){
+        this.minSteps = minSteps;
+        this.totalSteps = 0;
+        this.numDays = 0;
+        this.numActiveDays = 0;
+    }
+
 }
