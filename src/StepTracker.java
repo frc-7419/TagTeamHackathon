77f120ffb86ff76bbe8d6d753461e9f81b1f7860
kyle-du <kyledu123@gmail.com>
2023-09-13 18:11:56 -0700

    added merge conflicts

---
+++ src/StepTracker.java
@@ -0,0 +1,10 @@
+public class StepTracker {
+    int exampleVar;
+    int exampleVar2;
+    int exampleVar3;
+    int exampleVar4;
+
+    public static String exampleMethod(int i) {
+        return "good stuff";
+    }
+}
