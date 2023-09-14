b3d6f5af2185eed091d00091d4735a444ed4309f
Astrogamer54 <astrogamer543@gmail.com>
2023-09-14 16:25:52 -0700

    steptrack

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -2,5 +2,12 @@ public class StepTracker {
     private int minSteps;
     private int totalSteps;
     private int numDays;
-    private int numActiveDAys;
+    private int numActiveDays;
+
+    public StepTracker(int threshold){ 
+        minSteps = threshold;
+        totalSteps = 0;
+        numDays = 0;
+        numActiveDays = 0;
+    }
 }
