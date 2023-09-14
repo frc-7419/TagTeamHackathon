1a3ed7119d3685db2d2aacf076f45ae1d86f5b47
rqhu2025 <rhustudy@gmail.com>
2023-09-14 16:22:15 -0700

    add construcotr
    yee

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -3,5 +3,10 @@ public class StepTracker {
     private int totalSteps;
     private int numDays;
     private int numActiveDays;
-    
+    public StepTracker(int threshold) {
+        minStep = threshold;
+        totalSteps = 0;
+        numDays = 0;
+        numActiveDays = 0;
+    }
 }
