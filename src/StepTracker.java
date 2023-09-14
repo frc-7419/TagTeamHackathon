5a1db069b9f8d2326ad5939d8d6ee139b1dbf0bd
SwimmingSushi <76238065+SwimmingSushi@users.noreply.github.com>
2023-09-14 16:28:41 -0700

    init

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -4,13 +4,14 @@ public class StepTracker {
     private int numDays;
     private int numActiveDays;
 
-    public StepTracker(int threshold){ 
+    public StepTracker(int threshold) {
         minSteps = threshold;
         totalSteps = 0;
         numDays = 0;
         numActiveDays = 0;
     }
-    public void addDailySteps(int steps){
-        
+
+    public void addDailySteps(int steps) {
+        totalSteps += steps;
     }
 }
