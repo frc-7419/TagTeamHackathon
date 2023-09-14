1abf4056792622957b1034b9ae8895ed331e4919
Karthikeya Atmakuru <karthikeya.atmakuru@gmail.com>
2023-09-14 16:25:28 -0700

    added private ints

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,5 +1,9 @@
 public class StepTracker {
+    private int minSteps;
+    private int totalSteps;
+    private int numDays;
+    private int numActiveDays;
     public static void main(String[] args) {
 
-    }
+        }
 }
