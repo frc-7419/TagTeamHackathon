a5415ce39e93966a4cd7a2b2517f76c94a3f48a0
Angie-Feng-05 <angie.feng.shda@gmail.com>
2023-09-14 17:07:26 -0700

    added StepTracker

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -3,5 +3,14 @@ class StepTracker {
     private int totalSteps;
     private int numDays;
     private int numActiveDays;
+    
+    public StepTracker(int threshold){
+        minSteps = threshold;
+        int minSteps = 0;
+        int totalSteps = 0;
+        int numDays = 0;
+        int numActiveDays = 0;
+    }
 
 }
+
