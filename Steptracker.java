50d3b7c0972df92598ac70c49c0c9506a4708dab
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2024-09-03 16:52:39 -0700

    Update Steptracker.java
    Added a contructor which updated all variable values along with a threshold for minsteps

--- Steptracker.java
+++ Steptracker.java
@@ -3,4 +3,11 @@ public class Steptracker {
     private  int totalSteps;
     private int numDays;
     private int numActiveDays;
+
+    public Steptracker(int threshold){
+        this.minSteps = threshold;
+        this.totalSteps = 0;
+        this.numDays = 0;
+        this.numActiveDays = 0;
+    }
 }
