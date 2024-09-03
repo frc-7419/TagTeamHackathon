b22d92c03a5b9770799fa77a38d80e852f67445a
adhyayan-ai <69051467+adhyayan-ai@users.noreply.github.com>
2024-09-03 16:54:14 -0700

    Second

--- .vscode/StepTracker.java
+++ .vscode/StepTracker.java
@@ -2,5 +2,12 @@ public class StepTracker {
     private int minSteps;      
     private int totalSteps;   
     private int activeDays;    
-    private int days;          
+    private int days;         
+
+    public StepTracker(int threshold) {
+        this.minSteps = threshold;
+        this.totalSteps = 0;        
+        this.activeDays = 0;    
+        this.days = 0;             
+    }
 }
