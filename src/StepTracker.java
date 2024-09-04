f98d20d7137dc6c6b7058a6dd13e4d5e94fdd447
mqxs21 <maxshi239@gmail.com>
2024-09-03 17:10:13 -0700

    added everything

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,10 +1,14 @@
 public class StepTracker {
-    int exampleVar;
-    int exampleVar2;
-    int exampleVar3;
-    int exampleVar4;
+    private int minSteps;
+private int totalSteps;
+private int numDays;
+private int numActiveDays;
+public StepTracker(int threshold){
+    this.minSteps = threshold;
+    this.numDays = 0;
+    this.numActiveDays = 0;
+    this.totalSteps = 0;
+}
 
-    public static String exampleMethod(int i) {
-        return "good stuff";
-    }
+
 }
